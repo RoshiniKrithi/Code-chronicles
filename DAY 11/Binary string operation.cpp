0b6d6f5f7529e2f24400d91eb013ac9029f7ec94
A Roshini Krithi <roshinikrithi0@gmail.com>
2026-01-12 22:08:52 +0530

    Add files via upload

---
+++ DAY 11/Binary string operation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        string S;
+        cin >> S;
+
+        int operations = 0;
+        for (int i = 1; i < N; i++) {
+            if (S[i] == S[i - 1]) {
+                operations++;
+            }
+        }
+
+        cout << operations << endl;
+    }
+    return 0;
+}
